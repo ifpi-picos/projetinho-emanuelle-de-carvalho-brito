bb5e56916a7b626229e5260aaeaece2b1d5a82bf
Emanuelle de Carvalho Brito <emanuellybritto95@gmail.com>
2024-08-27 22:33:14 -0300

    Ajustando funções de exibição

--- funcoes_exibir.js
+++ funcoes_exibir.js
@@ -1,9 +1,12 @@
 import { tarefas, tarefasConcluidas } from "./listas"
 
+export function adicionandoVerificacao(text) {
+    return `${text} ✓`
+}
 
-export function converter(){
+export function converter() {
     tarefas.sort((objetoA, objetoB) => {
-            
+
         let dataA = objetoA.vencimento.split('/')
         let dataB = objetoB.vencimento.split('/')
 
@@ -11,11 +14,15 @@ export function converter(){
         let dateB = new Date(`${dataB[2]}-${dataB[1]}-${dataB[0]}`)
 
         return dateA - dateB
+
+
     })
-  
+
+
+
     console.log(`
         -- Tarefas pendentes --`)
-    for (let objeto of tarefas){
+    for (let objeto of tarefas) {
         console.log(`
            Título: ${objeto.titulo}
            Descrição: ${objeto.descricao}
@@ -27,36 +34,35 @@ export function converter(){
 
 
 
-
-export function exibirTarefas(){
-     console.log(`
+export function exibirTarefas() {
+    console.log(`
         -- Tarefas pendentes --`)
-     for (let pendente of tarefas){
-         console.log(`
+    for (let pendente of tarefas) {
+        console.log(`
              Título: ${pendente.titulo}
              Descrição: ${pendente.descricao}
              Vencimento: ${pendente.vencimento}
              Prioridade: ${pendente.prioridade}
              
              `)
-     }
+    }
 
-     console.log(`
+    console.log(`
         -- Tarefas concluídas --`)
-     for (let concluida of tarefasConcluidas){
-         console.log(`
+    for (let concluida of tarefasConcluidas) {
+        console.log(`
              Título: ${concluida.titulo}
              Descrição: ${concluida.descricao}
              Vencimento: ${concluida.vencimento}
              Prioridade: ${concluida.prioridade}
              
              `)
-     }
+    }
 }
 
 
 
-export function ordenar(){
+export function ordenar() {
     let ordenar = Number(prompt(`
         1 - Vencimento
         2 - Prioridade
@@ -64,18 +70,29 @@ export function ordenar(){
         
         `))
 
-    if (ordenar === 1){
+    if (ordenar === 1) {
         converter()
 
-    } else if (ordenar === 2){
 
-        tarefas.sort((a, b) => {a.prioridade - b.prioridade})
-        // Alterar ordenação
+
+    } else if (ordenar === 2) {
+
+        let ordemPrioridade = {
+            'alta': 1, 'media': 2, 'baixa': 3
+        }
+
+        tarefas.sort((a, b) => {
+            let prioridadeA = a.prioridade.toLowerCase()
+            let prioridadeB = b.prioridade.toLowerCase()
+
+            return ordemPrioridade[prioridadeA] - ordemPrioridade[prioridadeB]
+        })
+
         console.log(`
             -- Tarefas pendentes --
             `)
 
-        for (let prioridades of tarefas){
+        for (let prioridades of tarefas) {
             console.log(`
              Título: ${prioridades.titulo}
              Descrição: ${prioridades.descricao}
@@ -83,21 +100,56 @@ export function ordenar(){
              Prioridade: ${prioridades.prioridade}
                 `)
         }
-    } else{
-        for (let recente of tarefas){
+    } else {
+        let escolhaCriacao = Number(prompt(`
+            1 - Mais antigo 
+            2 - Mais recente
+            `))
+
+        if (escolhaCriacao === 1) {
+            let dataCriacao = new Date()
+
+
+            tarefas.sort((a, b) => {
+                return new Date(a.dataCriacao) - new Date(b.dataCriacao)
+            })
+
             console.log(`
-             Título: ${recente.titulo}
-             Descrição: ${recente.descricao}
-             Vencimento: ${recente.vencimento}
-             Prioridade: ${recente.prioridade}
+                -- Tarefas pendentes --
                 `)
+
+            for (let recente of tarefas) {
+                console.log(`
+                Título: ${recente.titulo}
+                Descrição: ${recente.descricao}
+                Vencimento: ${recente.vencimento}
+                Prioridade: ${recente.prioridade}
+                    `)
+            }
+        } else {
+            tarefas.sort((a, b) => {
+                return new Date(b.dataCriacao) - new Date(a.dataCriacao)
+            })
+
+            console.log(`
+                -- Tarefas pendentes --
+                `)
+
+            for (let recente of tarefas) {
+                console.log(`
+                Título: ${recente.titulo}
+                Descrição: ${recente.descricao}
+                Vencimento: ${recente.vencimento}
+                Prioridade: ${recente.prioridade}
+                    `)
+            }
         }
+
     }
 }
 
 
-
-export function filtrar(){
+export function filtrar() {
     let filtrar = Number(prompt(`
         1 - Pendentes
         2 - Concluídas
@@ -105,110 +157,148 @@ export function filtrar(){
         4 - Vencimento 
 
         `
-   ))
+    ))
 
-   if (filtrar === 1){
-       console.log(`
+    if (filtrar === 1) {
+        console.log(`
            -- Tarefas pendentes --
            `)
-       for (let filtarPendente of tarefas){
-           console.log(`
+        for (let filtarPendente of tarefas) {
+            console.log(`
                Titulo: ${filtarPendente.titulo}
                Descrição: ${filtarPendente.descricao}
                Vencimento: ${filtarPendente.vencimento}
                Prioridade: ${filtarPendente.prioridade}
 
                `)
-       }
-   } else if (filtrar === 2){
-       console.log(`
+        }
+    } else if (filtrar === 2) {
+        console.log(`
            -- Tarefas concluídas --
            `)
-       for (let filtarConcluida of tarefasConcluidas){
-           console.log(`
+        for (let filtarConcluida of tarefasConcluidas) {
+            console.log(`
                Titulo: ${filtarConcluida.titulo}
                Descrição: ${filtarConcluida.descricao}
                Vencimento: ${filtarConcluida.vencimento}
                Prioridade: ${filtarConcluida.prioridade}
 
                `
-           )
-   }
+            )
+        }
 
-}   else if(filtrar === 3){
-    let descobrirPrioridade = prompt('Digite a prioridade: ').trim().toLowerCase()
-            let procurarPrioridade = tarefas.filter((tarefa) => tarefa.prioridade.trim().toLowerCase() === descobrirPrioridade)
+    } else if (filtrar === 3) {
+        let descobrirPrioridade = prompt('Digite a prioridade: ').trim().toLowerCase()
 
-            if (procurarPrioridade.length > 0){
-                for (let filtarPrioridade of procurarPrioridade){
-                    console.log(`
+        let procurarPrioridade = tarefas.filter((tarefa) => tarefa.prioridade.trim().toLowerCase() === descobrirPrioridade)
+
+
+        if (procurarPrioridade.length > 0) {
+            for (let filtarPrioridade of procurarPrioridade) {
+                console.log(`
                         Titulo: ${filtarPrioridade.titulo}
                         Descrição: ${filtarPrioridade.descricao}
                         Vencimento: ${filtarPrioridade.vencimento}
                         Prioridade: ${filtarPrioridade.prioridade}
 
                         `
-                    )
-                }
+                )
             }
-}   else if(filtrar === 4){
-    
-    let buscarVencimento = Number(prompt(`
-        1 - Mais recente
-        2 - Menos recente 
-        `))
+        }
 
-    if (buscarVencimento === 1){
-        converter()
-    } else {
-        tarefas.sort((objetoA, objetoB) => {
-    
-            let dataA = objetoA.vencimento.split('/')
-            let dataB = objetoB.vencimento.split('/')
-    
-            let dateA = new Date(`${dataA[2]}-${dataA[1]}-${dataA[0]}`)
-            let dateB = new Date(`${dataB[2]}-${dataB[1]}-${dataB[0]}`)
-    
-            return dateB - dateA
-    })
+    } else if (filtrar === 4) {
+
+        let buscarVencimento = Number(prompt(`
+            1 - Mais recente
+            2 - Menos recente 
+            `))
+
+        if (buscarVencimento === 1) {
+            converter()
+        } else {
+
+            tarefas.sort((objetoA, objetoB) => {
+
+                let dataA = objetoA.vencimento.split('/')
+                let dataB = objetoB.vencimento.split('/')
+
+                let dateA = new Date(`${dataA[2]}-${dataA[1]}-${dataA[0]}`)
+                let dateB = new Date(`${dataB[2]}-${dataB[1]}-${dataB[0]}`)
+
+                return dateB - dateA
+            })
+
+            console.log(`
+                -- Tarefas pendentes --`)
+            for (let objeto of tarefas) {
+                console.log(`
+                   Título: ${objeto.titulo}
+                   Descrição: ${objeto.descricao}
+                   Prioridade: ${objeto.prioridade}
+                   Vencimento: ${objeto.vencimento}
+                   `)
+            }
+
+        }
+
+
+    }
 }
-}
-}
 
 
 
-
-export function marcarConcluida(){
+export function marcarConcluida() {
     let marcar = prompt('Qual tarefa deseja marcar como concluída? ')
     let procurarEmTarefas = tarefas.find((tarefa) => tarefa.titulo === marcar)
 
-    if(procurarEmTarefas != -1){
+    if (procurarEmTarefas) {
         tarefasConcluidas.push(procurarEmTarefas)
         tarefas.splice(procurarEmTarefas, 1)
         console.log('Tarefa marcada como concluída!')
-        console.log(tarefasConcluidas)
-        
-    } else{
+
+        console.log(`
+            -- Tarefas concluídas --
+            `)
+        for (let concluirTarefa of tarefasConcluidas) {
+            console.log(adicionandoVerificacao(concluirTarefa.titulo))
+           
+        }
+
+    } else {
         console.log('Tarefa não encontrada!')
     }
 }
 
 
 
- 
-export function pesquisar(){
+
+export function pesquisar() {
     let pesquisarTarefa = prompt('Qual tarefa deseja procurar? ')
 
-    let procurandoTarefa = tarefas.find((tarefa) => tarefa.titulo === pesquisarTarefa)
+    let buscaParcial = tarefas.filter((tarefa =>
+        tarefa.titulo.toLowerCase().includes(pesquisarTarefa.toLowerCase()) || 
+        tarefa.descricao.toLowerCase().includes(pesquisarTarefa.toLowerCase())
+    ))
 
-    if(procurandoTarefa){
+    if (buscaParcial.length >= 1) {
         console.log('Essa tarefa está na sua lista!')
-    } else{
+        for (let tarefaEncontrada of buscaParcial){
+            console.log(`
+            Título: ${tarefaEncontrada.titulo}
+            Descrição: ${tarefaEncontrada.descricao}
+            Vencimento: ${tarefaEncontrada.vencimento}
+            Prioridade: ${tarefaEncontrada.prioridade}
+            `)
+        }
+        
+
+    } else {
         console.log('Essa tarefa não está na sua lista!')
     }
 }
 
-export function resumir(){
+export function resumir() {
+    
     console.log(`
         Total de tarefas: ${tarefas.length + tarefasConcluidas.length}
         Tarefas pendendes: ${tarefas.length}
@@ -217,11 +307,26 @@ export function resumir(){
         `)
 
     let dataAtual = new Date()
-    let novaData = new Date(dataAtual.getFullYear(), dataAtual.getMonth(), dataAtual.getDate())
-    let proximaTarefa = new Date(tarefas.vencimento)
+
+    console.log(`
+        --- Próximas tarefas a vencer ---
+        `)
     
-    if (proximaTarefa <= novaData){
-        console.log('oi')
+    for (let proximoVencimento of tarefas) {
+        let [dia, mes, ano] = proximoVencimento.vencimento.split('/');
+        let proximaTarefa = new Date(`${ano}-${mes}-${dia}`)
+        
+        if (proximaTarefa >= dataAtual) {
+            console.log(`
+                    Titulo: ${proximoVencimento.titulo}
+                    Descrição: ${proximoVencimento.descricao}
+                    Vencimento: ${proximoVencimento.vencimento}
+                    Prioridade: ${proximoVencimento.prioridade}
+
+                        `
+            )
+        }
     }
-    
 }
+        
+    
